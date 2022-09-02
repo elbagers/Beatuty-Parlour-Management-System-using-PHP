4a94c96db20d8053b9beb7e73c9bfd4ef3779225
elbagers <112570615+elbagers@users.noreply.github.com>
2022-09-01 21:12:15 -0400

    Update bpmsdb.sql

--- Beatuty Parlour Management System Using PHP/SQL File/bpmsdb.sql
+++ Beatuty Parlour Management System Using PHP/SQL File/bpmsdb.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
---
+-- 
 -- Host: 127.0.0.1
 -- Generation Time: Jun 01, 2022 at 03:30 AM
 -- Server version: 10.4.22-MariaDB
